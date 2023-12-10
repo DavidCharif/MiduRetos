215936a7c1f5dcfee932390601930f3b380a4c98
Davidm <davidcharif@hotmail.com>
2023-12-10 14:35:30 -0500

    ts

---
+++ Reto 8/script.ts
@@ -0,0 +1,47 @@
+function organizeGifts (gifts: String) {
+  // Code here
+  let string = ''
+  const lettersFromGift = extractNumbersAndLetters(gifts)
+  lettersFromGift.letters.forEach((letter, index) => {
+    const number = lettersFromGift.numbers[index]
+    string += reduceGifts(letter, number)
+  })
+  function extractNumbersAndLetters (str) {
+    const numbers = str.match(/\d+/g).map(Number)
+    const letters = str.match(/[a-zA-Z]+/g)
+    return { numbers, letters }
+  }
+  function reduceGifts (letter, giftsNumber, res = '') {
+    while (giftsNumber !== 0) {
+      if (giftsNumber >= 50) {
+        res += `[${letter}]`
+        giftsNumber -= 50
+      } else if (giftsNumber >= 10) {
+        giftsNumber -= 10
+        res += `{${letter}}`
+      } else if (giftsNumber > 1) {
+        giftsNumber -= 1
+        if (res.includes('(')) {
+          res += letter
+        } else {
+          res += `(${letter}`
+        }
+      } else if (giftsNumber === 1) {
+        giftsNumber -= 1
+        if (!res.includes('(')) {
+          res += `(${letter})`
+        } else if (!res.includes(')')) {
+          res += `${letter})`
+        } else {
+          res += `${letter}`
+        }
+      }
+    }
+    return res
+  }
+  //
+
+  return string
+}
+
+module.exports = { organizeGifts }
