df0644c8ce3627ce58a329b4b1e703d54ac9ada7
Davidm <davidcharif@hotmail.com>
2023-12-10 14:27:16 -0500

    Formated and added export

--- Reto 8/script.js
+++ Reto 8/script.js
@@ -1,45 +1,47 @@
-function organizeGifts(gifts) {
+function organizeGifts (gifts) {
   // Code here
-  let string = "";
-  const lettersFromGift = extractNumbersAndLetters(gifts);
+  let string = ''
+  const lettersFromGift = extractNumbersAndLetters(gifts)
   lettersFromGift.letters.forEach((letter, index) => {
-    let number = lettersFromGift.numbers[index];
-    string += reduceGifts(letter, number);
-  });
-  function extractNumbersAndLetters(str) {
-    let numbers = str.match(/\d+/g).map(Number);
-    let letters = str.match(/[a-zA-Z]+/g);
-    return { numbers, letters };
+    const number = lettersFromGift.numbers[index]
+    string += reduceGifts(letter, number)
+  })
+  function extractNumbersAndLetters (str) {
+    const numbers = str.match(/\d+/g).map(Number)
+    const letters = str.match(/[a-zA-Z]+/g)
+    return { numbers, letters }
   }
-  function reduceGifts(letter, giftsNumber, res = "") {
+  function reduceGifts (letter, giftsNumber, res = '') {
     while (giftsNumber !== 0) {
       if (giftsNumber >= 50) {
-        res += `[${letter}]`;
-        giftsNumber -= 50;
+        res += `[${letter}]`
+        giftsNumber -= 50
       } else if (giftsNumber >= 10) {
-        giftsNumber -= 10;
-        res += `{${letter}}`;
+        giftsNumber -= 10
+        res += `{${letter}}`
       } else if (giftsNumber > 1) {
-        giftsNumber -= 1;
-        if (res.includes("(")) {
-          res += letter;
+        giftsNumber -= 1
+        if (res.includes('(')) {
+          res += letter
         } else {
-          res += `(${letter}`;
+          res += `(${letter}`
         }
       } else if (giftsNumber === 1) {
-        giftsNumber -= 1;
-        if (!res.includes("(")) {
-          res += `(${letter})`;
-        } else if (!res.includes(")")) {
-          res += `${letter})`;
+        giftsNumber -= 1
+        if (!res.includes('(')) {
+          res += `(${letter})`
+        } else if (!res.includes(')')) {
+          res += `${letter})`
         } else {
-          res += `${letter}`;
+          res += `${letter}`
         }
       }
     }
-    return res;
+    return res
   }
   //
 
-  return string;
+  return string
 }
+
+module.exports = { organizeGifts }
